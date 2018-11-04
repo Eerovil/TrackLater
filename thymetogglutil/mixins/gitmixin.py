9aa7c3ae2168c39cb793ef8d7d2c71ba5d4b4b37
Eero Vilpponen <eero.vilpponen@gmail.com>
2018-11-04 13:51:24 +0200

    Limit git commits to start_date/end_date

--- thymetogglutil/mixins/gitmixin.py
+++ thymetogglutil/mixins/gitmixin.py
@@ -1,8 +1,12 @@
 from thymetogglutil import settings
 import git
+import pytz
 
 from datetime import datetime
 from thymetogglutil.utils import FixedOffset
+
+
+HEL = pytz.timezone('Europe/Helsinki')
 
 
 class GitMixin(object):
@@ -11,6 +15,8 @@ class GitMixin(object):
         self.log = []  # List of git.refs.log.RefLogEntry objects
 
     def parse_git(self):
+        start_date = self.start_date.replace(tzinfo=HEL)
+        end_date = self.end_date.replace(tzinfo=HEL)
         for repo_path in self.repos:
             repo = git.Repo(repo_path)
             for branch in repo.branches:
@@ -20,12 +26,15 @@ class GitMixin(object):
                     if not log_entry.message.startswith('commit'):
                         continue
                     message = ''.join(log_entry.message.split(':')[1:])
+                    time = datetime.fromtimestamp(
+                        log_entry.time[0], tz=FixedOffset(log_entry.time[1], 'Helsinki')
+                    )
+                    if time < start_date or time > end_date:
+                        continue
                     self.log.append({
                         'repo': repo_path.split('/')[-1],
                         'message': message,
-                        'time': datetime.fromtimestamp(
-                            log_entry.time[0], tz=FixedOffset(log_entry.time[1], 'Helsinki')
-                        ),
+                        'time': time,
                     })
 
     def get_commits(self, start_time, end_time):
