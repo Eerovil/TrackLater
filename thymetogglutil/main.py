c0f368b7515e5bd2e40ebb904f3142e94e6e9aa7
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-18 15:18:00 +0300

    Type hints

--- thymetogglutil/main.py
+++ thymetogglutil/main.py
@@ -3,7 +3,11 @@
 
 
 import importlib
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, Future
+
+from timemodules.interfaces import AbstractParser
+from typing import Dict, Tuple, Optional, List, Any
+from types import ModuleType
 
 import settings
 from time import sleep
@@ -13,20 +17,22 @@ logger = logging.getLogger(__name__)
 
 
 class Parser(object):
-    def __init__(self, start_date, end_date):
+    def __init__(self, start_date, end_date) -> None:
         self.start_date = start_date
         self.end_date = end_date
-        self.modules = {}
+        self.modules: Dict[str, AbstractParser] = {}
 
-    def parse(self):
+    def parse(self) -> None:
         with ThreadPoolExecutor() as executor:
-            running_tasks = []
+            running_tasks: List[Tuple[str, Optional[Future[Any]]]] = []
 
             for module_name in settings.ENABLED_MODULES:
-                module = importlib.import_module(
+                module: ModuleType = importlib.import_module(
                     'thymetogglutil.timemodules.{}'.format(module_name)
                 )
-                parser = module.Parser(self.start_date, self.end_date)
+                if getattr(module, 'Parser', None) is None:
+                    logger.warning('Module %s has no Parser class', module_name)
+                parser = module.Parser(self.start_date, self.end_date)  # type: ignore
                 self.modules[module_name] = parser
                 running_tasks.append((module_name, executor.submit(parser.parse)))
                 logger.warning("Parsing %s", module_name)
