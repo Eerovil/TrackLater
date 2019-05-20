1bb22cf4169415071af7e9174b3917fcd9b668f8
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-20 07:04:32 +0300

    #4 Create helper fixture to generate data from fixture.
    This function can generate an object from a dict, along with callables.

--- tracklater/tests/conftest.py
+++ tracklater/tests/conftest.py
@@ -25,3 +25,41 @@ def mock_settings(monkeypatch):
             'PROJECT_KEY': 'TEST',
         }
     })
+
+
+@pytest.fixture
+def obj_from_dict():
+    def obj_dic(d):
+        top = type('new', (object,), d)
+        seqs = tuple, list, set, frozenset
+        callables = {}
+        for i, j in d.items():
+            if isinstance(j, dict):
+                value = obj_dic(j)
+            elif isinstance(j, seqs):
+                value = type(j)(obj_dic(sj) if isinstance(sj, dict) else sj for sj in j)
+            else:
+                value = j
+            _match = re.match('(.*)\((.*)\)', i)  # noqa
+            if _match:
+                _key = _match.groups()[0]
+                # Matched parentheses, so we will need to build a function (later)
+                # for _key
+                _args = []
+                if len(_match.groups()) > 1 and _match.groups()[1] != '':
+                    _args = _match.groups()[1].split(",")
+                callables[_key] = callables.get(_key, [])
+                # Store a list of possible arguments and their corresponding value
+                callables[_key].append((_args, value))
+            else:
+                setattr(top, i, value)
+
+        for _key, arg_list in callables.items():
+            def _func(*args, **kwargs):
+                for _data in arg_list:
+                    if list(args[:len(_data[0])]) == _data[0]:
+                        return _data[1]
+            setattr(top, _key, _func)
+
+        return top
+    return obj_dic
