514218c74700e785ceb6b2dcc7417a44b09a88ac
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-22 15:23:36 +0300

    #4 Implement provider in taiga

--- tracklater/timemodules/taiga.py
+++ tracklater/timemodules/taiga.py
@@ -1,7 +1,7 @@
 import requests
 import settings
 
-from timemodules.interfaces import IssueMixin, AbstractParser, Issue
+from timemodules.interfaces import IssueMixin, AbstractParser, Issue, AbstractProvider
 
 from typing import List, Dict, Any
 
@@ -31,18 +31,7 @@ class Parser(IssueMixin, AbstractParser):
         return issues
 
     def taiga_login(self) -> None:
-        credentials = taiga_settings['global']['CREDENTIALS']
-        response = requests.post(
-            AUTH_URL, data={
-                'type': 'normal',
-                'username': credentials[0],
-                'password': credentials[1]
-            }
-        )
-        token = response.json()['auth_token']
-        self.headers = {
-            "Authorization": "Bearer {}".format(token), 'x-disable-pagination': 'True'
-        }
+        self.provider = Provider(taiga_settings['global']['CREDENTIALS'])
         self.issues = []
         self.taiga_projects: List[dict] = []
         # Get taiga project id for all clients
@@ -50,23 +39,73 @@ class Parser(IssueMixin, AbstractParser):
         for group, data in taiga_settings.items():
             if 'project_slug' not in data:
                 continue
-            response = requests.get(
-                PROJECT_URL.format(data['project_slug']),
-                headers=self.headers
-            )
+            project = self.provider.get_project(data['project_slug'])
             self.taiga_projects.append({
-                'id': response.json()['id'],
+                'id': project['id'],
                 'group': group
             })
 
     def taiga_fetch_issues(self, start_from=None):
         issues: List[dict] = []
         for taiga_project in self.taiga_projects:
-            response = requests.get(
-                '{ISSUE_URL}?project={id}'.format(
-                    ISSUE_URL=ISSUE_URL,
-                    id=taiga_project['id']
-                ), headers=self.headers
-            )
-            issues += response.json()
+            issues += self.provider.get_issues(taiga_project['id'])
         return issues
+
+
+class Provider(AbstractProvider):
+    def __init__(self, credentials):
+        self.token = self.login(credentials)
+        self.headers = {
+            "Authorization": "Bearer {}".format(self.token), 'x-disable-pagination': 'True'
+        }
+
+    def login(self, credentials):
+        response = requests.post(
+            AUTH_URL, data={
+                'type': 'normal',
+                'username': credentials[0],
+                'password': credentials[1]
+            }
+        )
+        return response.json()['auth_token']
+
+    def test_login(self, credentials):
+        return ""
+
+    def get_project(self, project_slug):
+        response = requests.get(
+            PROJECT_URL.format(project_slug),
+            headers=self.headers
+        )
+        return response.json()
+
+    def test_get_project(self, credentials):
+        return {'id': "1"}
+
+    def get_issues(self, project_id):
+        response = requests.get(
+            '{ISSUE_URL}?project={id}'.format(
+                ISSUE_URL=ISSUE_URL,
+                id=project_id
+            ), headers=self.headers
+        )
+        return response.json()
+
+    def test_get_issues(self, credentials):
+        return [
+            {
+                "ref": "1",
+                "subject": "Taiga issue 1",
+                "project": "1"
+            },
+            {
+                "ref": 2,
+                "subject": "Taiga issue 2",
+                "project": "1"
+            },
+            {
+                "ref": 3,
+                "subject": "Taiga issue 3",
+                "project": "1"
+            }
+        ]
