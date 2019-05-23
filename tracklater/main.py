3185577521461c2f7cea00322224282bb01317ef
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-23 07:19:18 +0300

    #4 Add traceback to threads

--- tracklater/main.py
+++ tracklater/main.py
@@ -4,16 +4,26 @@
 
 import importlib
 import settings
-from concurrent.futures import ThreadPoolExecutor, Future
+from concurrent.futures import ThreadPoolExecutor, Future, as_completed
 
 from timemodules.interfaces import AbstractParser
 from typing import Dict, Tuple, Optional, List, Any
 from types import ModuleType
 
-from time import sleep
-
+import traceback
 import logging
 logger = logging.getLogger(__name__)
+
+
+def reraise_with_stack(func):
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            traceback_str = traceback.format_exc()
+            raise Exception("Error occurred. Original traceback "
+                            "is\n%s\n" % traceback_str)
+    return wrapped
 
 
 class Parser(object):
@@ -24,7 +34,7 @@ class Parser(object):
 
     def parse(self) -> None:
         with ThreadPoolExecutor() as executor:
-            running_tasks: List[Tuple[str, Optional[Future[Any]]]] = []
+            running_tasks = {}
 
             for module_name in settings.ENABLED_MODULES:
                 module: ModuleType = importlib.import_module(
@@ -34,16 +44,15 @@ class Parser(object):
                     logger.warning('Module %s has no Parser class', module_name)
                 parser = module.Parser(self.start_date, self.end_date)  # type: ignore
                 self.modules[module_name] = parser
-                running_tasks.append((module_name, executor.submit(parser.parse)))
+                running_tasks[module_name] = executor.submit(reraise_with_stack(parser.parse))
                 logger.warning("Parsing %s", module_name)
                 parser = None
-            running = True
-            while running:
-                running = False
-                for i, (module_name, running_task) in enumerate(running_tasks):
-                    if running_task and not running_task.running():
-                        logger.warning("Task done %s", module_name)
-                        running_tasks[i] = (module_name, None)
-                    elif running_task:
-                        running = True
-                sleep(0.1)
+
+            for task in as_completed([value for key, value in running_tasks.items()]):
+                for key, value in running_tasks.items():
+                    if value is task:
+                        module_name = key
+                        break
+                if task.exception():
+                    logger.exception("Exception in %s: %s", module_name, task.exception())
+                logger.warning("Task done %s", module_name)
