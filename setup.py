41a134ec7102df221fe2bab383f090389c4418f2
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-24 23:47:28 +0300

    flake8 fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
    name='tracklater',
    version='1.0',
-   description='TrackLater helps you track time after-the-fact by combining clues and showing your day on a simple timeline view.',
+   description=('TrackLater helps you track time after-the-fact by combining clues and showing'
+                'your day on a simple timeline view.'),
    author='Eero Vilpponen',
    author_email='eero.vilpponen@gmail.com',
    packages=['tracklater'],
