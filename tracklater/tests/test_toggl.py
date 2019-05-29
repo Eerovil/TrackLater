f32b12f3a9b01626c004077b46e31be1eb8e37f7
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-29 14:40:28 +0300

    Add back toggl update tests

--- tracklater/tests/test_toggl.py
+++ tracklater/tests/test_toggl.py
@@ -4,6 +4,7 @@ import pytest
 import os
 
 from datetime import datetime, timedelta
+from models import Entry
 
 DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
@@ -26,3 +27,22 @@ def test_toggl_get_projects(parser):
     """
     data = parser.get_projects()
     assert len(data) == 4
+
+
+def test_toggl_add_modify_delete(parser: Parser):
+    parser.entries = parser.get_entries()
+    entry = Entry(
+        id="4",
+        start_time=datetime.utcnow() - timedelta(hours=2),
+        end_time=datetime.utcnow() - timedelta(hours=1),
+        title="Toggl new entry (4)",
+        project="10",
+    )
+    entry = parser.create_entry(entry, None)
+    assert entry.id == "4"
+
+    entry.title = "Toggl modified entry"
+    entry = parser.update_entry("4", entry, None)
+    assert entry.title == "Toggl modified entry"
+
+    parser.delete_entry("4")
