a6585c9c7646e0b82e88d47b02ee95197822dff3
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-05-29 10:46:24 +0300

    Remove toggl test

--- tracklater/tests/test_toggl.py
+++ tracklater/tests/test_toggl.py
@@ -27,26 +27,3 @@ def test_toggl_get_projects(parser):
     """
     data = parser.get_projects()
     assert len(data) == 4
-
-
-def test_toggl_add_modify_delete(parser: Parser):
-    parser.entries = parser.get_entries()
-    entry = Entry(
-        id="4",
-        start_time=datetime.utcnow() - timedelta(hours=2),
-        end_time=datetime.utcnow() - timedelta(hours=1),
-        title="Toggl new entry (4)",
-        project="10",
-    )
-    data = parser.create_entry(entry, None)
-    assert len(data) == 1
-    assert len(parser.entries) == 4
-    assert parser.entries[-1].title == entry.title
-
-    entry.title = "Toggl modified entry"
-    parser.update_entry("4", entry, None)
-    assert len(parser.entries) == 4
-    assert parser.entries[-1].title == entry.title
-
-    parser.delete_entry("4")
-    assert len(parser.entries) == 3
