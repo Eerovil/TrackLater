0b80913284ab97d7650d2fbcef61f562daf996ec
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-06-03 15:17:50 +0300

    Implement issue selector, no functionality

--- tracklater/static/toolbar.vue.js
+++ tracklater/static/toolbar.vue.js
@@ -9,9 +9,38 @@ var toolbar = Vue.component("toolbar", {
       v-on:click=fetchModule(module)
       :loading="modules[module].loading"
     >{{ module }}</v-btn>
+    <br>
+    <v-combobox
+      v-model="entryTitle"
+      :items="allIssues"
+    >
+    </v-combobox>
     </div>
     `,
     props: ["modules"],
+    computed: {
+        allIssues() {
+            let ret = [];
+            for (let module_name in this.modules) {
+                const _issues = this.modules[module_name].issues || [];
+                for (let i=0; i<_issues.length; i++) {
+                    ret.push(`${_issues[i].key} ${_issues[i].title}`);
+                }
+            }
+            return ret;
+        },
+        selectedIssue() {
+            for (let module_name in this.modules) {
+                const _issues = this.modules[module_name].issues || [];
+                for (let i=0; i<_issues.length; i++) {
+                    if (`${_issues[i].key} ${_issues[i].title}` === this.entryTitle) {
+                        return _issues[i];
+                    }
+                }
+            }
+            return null;
+        }
+    },
     methods: {
         fetchModule(module_name) {
             this.$emit('fetchModule', module_name)
@@ -23,6 +52,8 @@ var toolbar = Vue.component("toolbar", {
         }
     },
     data() {
-        return {}
+        return {
+            entryTitle: null,
+        }
     }
 });
