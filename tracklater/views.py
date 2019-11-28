0268c21c3bae4ae117b66b7116e85a3968d27160
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-28 08:13:38 +0200

    Increase days shown

--- tracklater/views.py
+++ tracklater/views.py
@@ -59,7 +59,7 @@ def fetchdata() -> Optional[str]:
         if 'from' in request.values:
             from_date = parseTimestamp(request.values['from'])
         else:
-            from_date = now - timedelta(days=14)
+            from_date = now - timedelta(days=21)
         if 'to' in request.values:
             to_date = parseTimestamp(request.values['to'])
         else:
