ddd74a6febdc4f5a93989ad5f671fe88ab670762
Eero Vilpponen <eero.vilpponen@gmail.com>
2022-09-04 05:42:34 +0000

    Add test...

---
+++ tracklater/tests/test_activitywatch.py
@@ -0,0 +1,20 @@
+from tracklater.timemodules.activitywatch import Parser
+
+import pytest
+import os
+from datetime import datetime, timedelta
+
+DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.fixture()
+def parser():
+    _parser = Parser(datetime.utcnow() - timedelta(days=7), datetime.utcnow())
+    return _parser
+
+
+def test_get_entries(parser):
+    """
+    """
+    data = parser.get_entries()
+    assert len(data) == 1
